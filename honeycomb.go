433cd3d20ac7b2fd1e334eaa5caddab4a06f6c56
saaspanel <obie@Tres-MacBook-Pro.local>
2020-06-27 18:23:15 -0400

    corrected hasura query execution time prop name

--- honeycomb.go
+++ honeycomb.go
@@ -19,7 +19,7 @@ import (
 const (
 	DefaultHoneycombAPIURL = "https://api.honeycomb.io"
 	DefaultSampleRate      = 1
-	Version                = "v0.0.14"
+	Version                = "v0.0.15"
 )
 
 func init() {
@@ -199,7 +199,7 @@ func (a *HoneycombAdapter) Stream(logstream chan *router.Message) {
 
 				// convert query execution time from seconds to milliseconds
 				if queryExecutionTime := operation.(map[string]interface{})["query_execution_time"]; queryExecutionTime != nil {
-					data["hasura_query_execution_time"] = queryExecutionTime.(float64) * 1000
+					data["hasura_query_execution_time_in_ms"] = queryExecutionTime.(float64) * 1000
 				}
 			} else if query, ok2 := d["query"]; ok2 { // adapt hasura query log
 				requestID, _ := d["request_id"].(string)
